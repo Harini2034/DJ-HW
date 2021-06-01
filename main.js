a0aee3ee99f1a9d14580a1ef2bbdf177a281559d
Alexandria <72262425+Harini2034@users.noreply.github.com>
2021-06-01 19:06:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,40 @@
-song1 = "";
-song2 = "";
-leftWristX = 0;
-leftWistY =0;
-rightWristX = 0;
-rightWristY =0; 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AI DJ WEB APP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
-function preload()
-{
-   song1 = loadSound("Selena-Gomez-Wolves.mp3");
-   song2 = loadSound("Becky+G+Shower.mp3");
-}
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.0.0/p5.js"></script>
 
-function setup()
-{
-    canvas = createCanvas(600, 500);
-    canvas.center();
+  <script src="https://unpkg.com/ml5@0.4.3/dist/ml5.min.js"></script>
 
-    video = createCapture(VIDEO);
-    video.hide();
+  <link rel="stylesheet" type="text/css" href="style.css">
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.3.1/addons/p5.sound.min.js" integrity="sha512-wM+t5MzLiNHl2fwT5rWSXr2JMeymTtixiw2lWyVk1JK/jDM4RBSFoH4J8LjucwlDdY6Mu84Kj0gPXp7rLGaDyA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
-}
+</head>
+<body background="background.png">
+<center>
+  	<h3 class="btn btn-danger heading">AI DJ WEB APP
+	    <br>
+	    <span class="note">NOTE - Right hand = Beckcy.G - Shower. Left Hand = Selena Gomez - Wolves</span>
+  	</h3>
+    <div class="div_speed">
+      <img src="selena.gif" class="div_speed_img">
+      <br>
+      <h3 class="btn btn-danger" id="speed">Selena Gomez</h3>
+    </div>
 
-function modelLoaded()
-{
-    console.log('PoseNet Is Initialized');
-}
+    <div class="div_volume">
+      <img src="backyg.gif" class="div_volume_img">
+      <br>
+      <h3 class="btn btn-danger" id="volume"> Becky G </h3>
+    </div>
+	<br><br>
+</center>
 
-function draw()
-{
-   image(video, 0, 0, 600, 500);
-}
-
-function gotPoses(results)
-{
-    if(results.length > 0)
-    {
-        console.log(results);
-        leftWristX = results[0].pose.leftWrist.x;
-        leftWristY = results[0].pose.leftWrist.y;
-        console.log("leftWristX = " + leftWristX + "leftWristY =" + leftWristY);
-
-        rightWristX = results[0].pose.rightWrist.x;
-        rightWristY = results[0].pose.rightWrist.y;
-        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
-    }
-}
+<script src="main.js"></script>
+</body>
+</html>
